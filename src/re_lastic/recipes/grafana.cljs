c0feda2d8995d8c7bcb3926506cc81ca2448274f
ronen <narkisr.com>
2018-07-20 13:50:24 +0000

    kibana and grafana setup

---
+++ src/re_lastic/recipes/grafana.cljs
@@ -0,0 +1,11 @@
+(ns re-lastic.recipes.grafana
+  "Setting up Elasticsearch instance"
+  (:require-macros
+   [clojure.core.strint :refer (<<)])
+  (:require
+   [re-conf.resources.pkg :refer (package)]
+   [re-conf.resources.download :refer (download)]))
+
+(defn grafana
+  "Setting up Elasticsearch repo and package"
+  [])
---
+++ src/re_lastic/recipes/kibana.cljs
@@ -0,0 +1,11 @@
+(ns re-lastic.recipes.kibana
+  "Setting up Elasticsearch instance"
+  (:require-macros
+   [clojure.core.strint :refer (<<)])
+  (:require
+   [re-conf.resources.pkg :refer (package)]
+   [re-conf.resources.download :refer (download)]))
+
+(defn kibana
+  "Setting up Elasticsearch repo and package"
+  [])
