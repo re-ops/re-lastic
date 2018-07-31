e9a328942f4e6c0b18ca7947936661731a1e73f1
ronen <narkisr.com>
2018-07-31 13:46:38 +0000

    configuration options

--- src/re_lastic/recipes/elastic.cljs
+++ src/re_lastic/recipes/elastic.cljs
@@ -3,15 +3,20 @@
   (:require-macros
    [clojure.core.strint :refer (<<)])
   (:require
+   [re-conf.resources.file :refer (line)]
    [re-conf.resources.pkg :refer (package)]
    [re-conf.resources.service :refer (service)]
    [re-conf.resources.output :refer (summary)]))
 
 (defn elastic
   "Setting up Elasticsearch"
-  []
+  [{:keys [cluster node data]}]
   (->
    (package "openjdk-8-jre")
    (package "elasticsearch" :present)
-   (service "elasticsearch" :start)
+   (line "/etc/elasticsearch/elasticsearch.yml" "network.host: 0.0.0.0" :present)
+   (line "/etc/elasticsearch/elasticsearch.yml" (<< "cluster.name: ~{cluster}") :present)
+   (line "/etc/elasticsearch/elasticsearch.yml" (<< "node.name: ~{node}") :present)
+   (line "/etc/elasticsearch/elasticsearch.yml" "path.data" data ": " :set)
+   (service "elasticsearch" :restart)
    (summary "elastic setup done")))
