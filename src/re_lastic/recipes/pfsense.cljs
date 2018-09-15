070fa5d17e6e8907e7ff5cb7067325631630b821
ronen <narkisr@gmail.com>
2018-09-15 19:26:03 +1000

    adding ports and patterns symlink

--- src/re_lastic/recipes/pfsense.cljs
+++ src/re_lastic/recipes/pfsense.cljs
@@ -3,16 +3,29 @@
   (:require-macros
    [clojure.core.strint :refer (<<)])
   (:require
+   [re-conf.resources.firewall :refer (rule)]
    [re-conf.resources.git :refer (clone)]
    [re-conf.resources.file :refer (directory symlink)]
    [re-conf.resources.pkg :refer (package)]))
 
-(defn pfsense-logstash
-  "Setting up Pfsense Grok support"
+(defn pfsense-rules
+  "Ports required for Pfsense logstash"
   [c]
-  (let [conf-d "/etc/logstash/conf.d" pfsense "/etc/pfsense-kibana"]
+  (->
+   (rule {:port 5044}); filebeat snort
+   (rule {:port 5000}); syslog blocks
+))
+
+(defn pfsense-logstash
+  "Setting up Pfsense support"
+  [c]
+  (let [logstash "/etc/logstash/" pfsense "/etc/pfsense-kibana"]
     (->
-     (package c "git")
+     (pfsense-rules c)
+     (package "git")
      (directory conf-d :absent)
      (clone "git://github.com/narkisr/pfsense-kibana.git" pfsense)
-     (symlink (<< "~{pfsense}/conf.d") conf-d :present))))
+     (symlink (<< "~{pfsense}/conf.d") (<< "~{logstash}/conf.d") :present)
+     (symlink (<< "~{pfsense}/patterns") (<< "~{logstash}/patterns") :present)
+     )))
+
